d911947d55b35cd0218015de26b35b32889ff6af
Manuel <mbarbero@grupoinova.es>
2020-05-22 19:21:42 +0200

    Variable Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDb;
 if (process.env.NODE_ENV === 'dev') {
     urlDb = 'mongodb://cafe:Inovages2014@localhost:27017/cafe';
 } else {
-    urlDb = 'mongodb+srv://mabarsan:Mbs955095@cluster0-5d0vz.mongodb.net/cafe';
+    urlDb = process.env.MONGO_URI;
 }
 
 process.env.URL_BD = urlDb;
